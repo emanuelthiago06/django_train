d6ca3a796f80a188ad6c10ff4c210ca320643852
emanuelthiago06 <emanuel.thiago@academico.ifpb.edu.br>
2023-04-28 08:12:17 -0300

    i will finish the rest later

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
-
-from django.http import HttpResponse
-
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'galeria/index.html')
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
